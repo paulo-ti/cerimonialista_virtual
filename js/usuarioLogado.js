1a310e8cef5e2019050f1230e422151f809d505a
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2020-05-19 08:24:29 -0300

    Removido alert e console.log de testes

--- js/usuarioLogado.js
+++ js/usuarioLogado.js
@@ -1,6 +1,5 @@
 function verificarUsuarioLogado(){
     const auth = firebase.auth();
-    console.log(auth);
     auth.onAuthStateChanged(function(user) {
         if (!user) {
             window.location.href = "entrar.html";
