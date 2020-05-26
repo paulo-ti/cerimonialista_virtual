8ac4a4d296c14ec6552336503ba6abfb9a2b86fa
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2020-05-26 14:56:13 -0300

    Merge with lista-convidados

--- js/contratos.js
+++ js/contratos.js
@@ -1,17 +1,41 @@
+let $btnContratosSalvar = document.getElementById("btnContratosSalvar");
+let $descricaoContrato = document.getElementById("descricaoContrato");
+let $fileContrato = document.getElementById("fileContrato");
+let $listaContratos = document.getElementById("listaContratos");
+let $contratosList = document.getElementsByClassName("contratosList");
 
+$btnContratosSalvar.addEventListener("click", function () {
+  
+  let descricaoContrato = $descricaoContrato.value;
+  let fileContrato = $fileContrato.files[0].name;
 
-const refStorage = firebase.storage().ref('arquivos/contratos');
-let fileInput = document.getElementById('input-file');
+  let contrato = document.createTextNode(descricaoContrato);
+  let nomeContrato = document.createTextNode(fileContrato);
+  let tr = document.createElement("tr");
+  let th = document.createElement("th");
+  let td = document.createElement("td");
 
-fileInput.onchange = function(event){
-    let arquivo = event.target.files[0];
-    refStorage.child(idEventoGlobal+'/1').put(arquivo, {
-        customMetadata:{
-            descricao: "desc teste"
-        }
-    }).then(()=>{
-      alert('Arquivo Enviado com Sucesso!');
-    }).catch(err =>{
-        alert("Erro ao enviar o arquivo" + e);
-    })
-  }
+  $listaContratos.appendChild(tr);
+  tr.appendChild(th);
+  tr.appendChild(td);
+  th.classList.add("textTable");
+  tr.classList.add("contratosList");
+  td.classList.add("text-center");
+  th.appendChild(contrato);
+  td.appendChild(nomeContrato);
+ 
+  $descricaoContrato.value = "";
+  $fileContrato.value = ""
+  listReloader2()
+});
+
+function listReloader2() {
+  Array.prototype.forEach.call($contratosList, (contratosList) => {
+    contratosList.addEventListener("click", function (e) {
+      if (e.target.classList.value === "textTable") {
+        $listaContratos.lastElementChild.parentNode.removeChild(this);
+        alert(`O Contrato ${this.textContent} foi removido!`);
+      }
+    });
+  });
+}
