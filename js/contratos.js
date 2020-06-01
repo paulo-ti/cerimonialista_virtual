f79093f1b43c948605827fe6c74451bc1a052d15
paulo-ti <paulo-ti@hotmail.com>
2020-06-01 10:18:38 -0300

    seguindo sprint

--- js/contratos.js
+++ js/contratos.js
@@ -1,72 +1,90 @@
-(async function() {
-  
+(async function () {
   let $btnContratosSalvar = document.getElementById("btnContratosSalvar");
   let $listaContratos = document.getElementById("listaContratos");
   let $contratosList = document.getElementsByClassName("contratosList");
 
-  const refStorage = firebase.storage().ref('arquivos/contratos');
-  
-  let url_atual1 = window.location.href.split('?')
+  const refStorage = firebase.storage().ref("arquivos/contratos");
+
+  let url_atual1 = window.location.href.split("?");
   let idEvento = url_atual1[1];
-  
-  const refContratos = firebase.database().ref("EventoNovo"+"/"+idEvento).child("Contratos");
-  refContratos.once('value').then(function(snapshot){
+
+  const refContratos = firebase
+    .database()
+    .ref("EventoNovo" + "/" + idEvento)
+    .child("Contratos");
+  refContratos.once("value").then(function (snapshot) {
     var arrayKeys = Object.keys(snapshot.val());
-    
-    for(let i = 0; i < arrayKeys.length ; i++){
-      var nomeCompletoContrato = snapshot.child(arrayKeys[i]).val().nomeContrato;
 
-      refStorage.child(idEvento+'/'+nomeCompletoContrato).getMetadata().then(function(snapshot){
-        var nome = snapshot.customMetadata.nome;
-        var descricao = snapshot.customMetadata.descricao;
+    for (let i = 0; i < arrayKeys.length; i++) {
+      var nomeCompletoContrato = snapshot.child(arrayKeys[i]).val()
+        .nomeContrato;
 
-        refStorage.child(idEvento+'/'+snapshot.name).getDownloadURL().then(function(snapshot){
-          if(descricao == '')
-            adicionarContratoHTML(nome, nome, snapshot)
-          else
-            adicionarContratoHTML(descricao, nome, snapshot)
-        })
-      })
+      refStorage
+        .child(idEvento + "/" + nomeCompletoContrato)
+        .getMetadata()
+        .then(function (snapshot) {
+          var nome = snapshot.customMetadata.nome;
+          var descricao = snapshot.customMetadata.descricao;
+
+          refStorage
+            .child(idEvento + "/" + snapshot.name)
+            .getDownloadURL()
+            .then(function (snapshot) {
+              if (descricao == "") adicionarContratoHTML(nome, nome, snapshot);
+              else adicionarContratoHTML(descricao, nome, snapshot);
+            });
+        });
     }
   });
 
-
   $btnContratosSalvar.addEventListener("click", function (e) {
-    e.stopImmediatePropagation()
+    e.stopImmediatePropagation();
     var $descricaoContrato = document.getElementById("descricaoContrato");
-    var fileInput = document.getElementById('input-file');
+    var fileInput = document.getElementById("input-file");
 
     let descricaoContrato = $descricaoContrato.value;
     var fileContrato = fileInput.files[0].name;
-    if(descricaoContrato == "")
-      descricaoContrato = fileContrato; 
-    
+    if (descricaoContrato == "") descricaoContrato = fileContrato;
+
     let contrato = document.createTextNode(descricaoContrato);
     let nomeContrato = document.createTextNode(fileContrato);
-     
+
     let arquivo = fileInput.files[0];
-    let nomeCompletoContrato = fileContrato+'_'+$descricaoContrato.value;
-    const refStorageContrato = refStorage.child(idEvento+'/'+nomeCompletoContrato);
-    refStorageContrato.put(arquivo, {
-        customMetadata:{
-            nome: fileContrato,
-            descricao: $descricaoContrato.value
-        }
-    }).then((snapshot)=>{
-      firebase.database().ref("EventoNovo"+"/"+idEvento).child("Contratos").push().set({nomeContrato : nomeCompletoContrato}).then(function(){
-        console.log(snapshot)
-        refStorageContrato.getDownloadURL().then(function(url){
-          adicionarContratoHTML2(contrato, nomeContrato, url)
-        })
+    let nomeCompletoContrato = fileContrato + "_" + $descricaoContrato.value;
+    const refStorageContrato = refStorage.child(
+      idEvento + "/" + nomeCompletoContrato
+    );
+    refStorageContrato
+      .put(arquivo, {
+        customMetadata: {
+          nome: fileContrato,
+          descricao: $descricaoContrato.value,
+        },
       })
-      
-      
-    }).catch(err =>{
+      .then((snapshot) => {
+        firebase
+          .database()
+          .ref("EventoNovo" + "/" + idEvento)
+          .child("Contratos")
+          .push()
+          .set({ nomeContrato: nomeCompletoContrato })
+          .then(function () {
+            console.log(snapshot);
+            refStorageContrato.getDownloadURL().then(function (url) {
+              adicionarContratoHTML2(contrato, nomeContrato, url);
+              $descricaoContrato.value = "";
+              $input-file.value = "";
+              listReloader2();
+            });
+          });
+      })
+      .catch((err) => {
         alert("Erro ao enviar o arquivo");
         console.log(err);
-    })
-    listReloader2()
-
+      });
+    $descricaoContrato.value = "";
+    $fileContrato.value = "";
+    listReloader2();
   });
 
   function listReloader2() {
@@ -80,53 +98,53 @@
     });
   }
 
-  function deletarArquivoStorage(nomeArquivo){
-    refStorage.child(idEvento+'/'+nomeArquivo).delete().then(function(){
-      console.log(nomeArquivo+" deletado com sucesso")
-      removerArquivoDoHTML()
-    }).catch(error=>{
-      console.log(Erro+error)
-    })
-    
+  function deletarArquivoStorage(nomeArquivo) {
+    refStorage
+      .child(idEvento + "/" + nomeArquivo)
+      .delete()
+      .then(function () {
+        console.log(nomeArquivo + " deletado com sucesso");
+        removerArquivoDoHTML();
+      })
+      .catch((error) => {
+        console.log(Erro + error);
+      });
   }
-  function removerArquivoDoHTML(){
+  function removerArquivoDoHTML() {}
 
-  }
-
-  function adicionarContratoHTML(nome, descricao, url){
+  function adicionarContratoHTML(nome, descricao, url) {
     let tr = document.createElement("tr");
     let th = document.createElement("th");
-    let td = document.createElement("td"); 
-    let URL = document.createElement("td"); 
+    let td = document.createElement("td");
+    let URL = document.createElement("td");
     $listaContratos.appendChild(tr);
-      tr.appendChild(th);
-      tr.appendChild(td);
-      tr.appendChild(URL);
-      th.classList.add("textTable");
-      tr.classList.add("contratosList");
-      td.classList.add("text-center");
-      td.classList.add("text-center")
-      th.innerText = nome;
-      td.innerText = descricao;
-      URL.innerText = url;
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tr.appendChild(URL);
+    th.classList.add("textTable");
+    tr.classList.add("contratosList");
+    td.classList.add("text-center");
+    td.classList.add("text-center");
+    th.innerText = nome;
+    td.innerText = descricao;
+    listReloader2();
   }
 
-  function adicionarContratoHTML2(nome, descricao, url){
+  function adicionarContratoHTML2(nome, descricao, url) {
     let tr = document.createElement("tr");
     let th = document.createElement("th");
-    let td = document.createElement("td"); 
-    let URL = document.createElement("td"); 
+    let td = document.createElement("td");
+    let URL = document.createElement("td");
     $listaContratos.appendChild(tr);
-      tr.appendChild(th);
-      tr.appendChild(td);
-      tr.appendChild(URL);
-      th.classList.add("textTable");
-      tr.classList.add("contratosList");
-      td.classList.add("text-center");
-      td.classList.add("text-center")
-      th.appendChild(nome)
-      td.appendChild(descricao);
-      URL.innerText = url;
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tr.appendChild(URL);
+    th.classList.add("textTable");
+    tr.classList.add("contratosList");
+    td.classList.add("text-center");
+    td.classList.add("text-center");
+    th.appendChild(nome);
+    td.appendChild(descricao);
   }
 
   // fileInput.onchange = function(event){
@@ -140,6 +158,5 @@
   //   }).catch(err =>{
   //       alert("Erro ao enviar o arquivo" + e);
   //   })
-  // } 
-
-})()
+  // }
+})();
