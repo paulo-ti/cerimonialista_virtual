bc0ceb7e08abd85ce701ad1075d01bb124ec8a59
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2020-06-04 13:19:30 -0300

    insert, select e delete em lista-convidados

--- js/lista_convidados.js
+++ js/lista_convidados.js
@@ -1,73 +1,113 @@
-let $btnAdicionarConvidados = document.getElementById("btnAdicionarConvidados");
-let $inputNomeConvidado = document.getElementById("inputNomeConvidado");
-let $inputConfirmado = document.getElementById("inputConfirmado");
-let $listaConvidados = document.getElementById("listaConvidados");
-let $checks = document.getElementsByClassName("checks");
-let $qtdTotal = document.querySelector("#qtdTotal");
-let $qtdConfirmados = document.querySelector("#qtdConfirmados");
-let $namesList = document.getElementsByClassName("namesList");
+(function () {
+  let $btnAdicionarConvidados = document.getElementById("btnAdicionarConvidados");
+  let $inputNomeConvidado = document.getElementById("inputNomeConvidado");
+  let $inputConfirmado = document.getElementById("inputConfirmado");
+  let $listaConvidados = document.getElementById("listaConvidados");
+  let $checks = document.getElementsByClassName("checks");
+  let $qtdTotal = document.querySelector("#qtdTotal");
+  let $qtdConfirmados = document.querySelector("#qtdConfirmados");
+  let $namesList = document.getElementsByClassName("namesList");
+  let url_atual1 = window.location.href.split("?");
+  let idEvento = url_atual1[1];
 
-$btnAdicionarConvidados.addEventListener("click", function () {
-  let nomeConvidado = $inputNomeConvidado.value;
-  let inputConfirmado = $inputConfirmado.checked;
+  referenciaBanco = firebase.database().ref("EventoNovo"+"/"+idEvento+"/"+"Convidados");
+  referenciaBanco.once("value").then(function(snapshot) {
+    snapshot.forEach(value =>{
+      var confirmado = value.val().confirmado
+      var nomeConvidado = value.val().nomeConvidado
+      montaHTML(nomeConvidado, confirmado, value.key );
+    })
+  })
 
-  let nome = document.createTextNode(nomeConvidado);
-  let tr = document.createElement("tr");
-  let th = document.createElement("th");
-  let td = document.createElement("td");
-  let checkbox = document.createElement("input");
+  $btnAdicionarConvidados.addEventListener("click", function () {
+    let nomeConvidado = $inputNomeConvidado.value;
+    let inputConfirmado = $inputConfirmado.checked;
+    referenciaBanco.push().set(
+       { nomeConvidado: nomeConvidado, confirmado: inputConfirmado }
+    ).then(function(snapshot){
+      montaHTML(nomeConvidado, inputConfirmado, snapshot.key)
+    })
+  });
 
-  $listaConvidados.appendChild(tr);
-  tr.appendChild(th);
-  tr.appendChild(td);
-  th.classList.add("textTable");
-  tr.classList.add("namesList");
-  td.classList.add("text-center");
-  checkbox.type = "checkbox";
-  checkbox.classList.add("checks");
-  checkbox.checked = inputConfirmado;
-  th.appendChild(nome);
-  td.appendChild(checkbox);
-  accountantGuests($checks);
-  accountantChecked($checks);
-  listReloader();
-
-  $inputNomeConvidado.value = "";
-  $inputConfirmado.checked = false;
-});
-
-function accountantGuests(list) {
-  let totalConvidados = list.length;
-  $qtdTotal.textContent = totalConvidados;
-}
-
-function accountantChecked(list) {
-  let x = 0;
-  let count = 0;
-
-  while (list.length > x) {
-    if (list[x].checked === true) {
-      count++;
-    }
-    x++;
+  function accountantGuests(list) {
+    let totalConvidados = list.length;
+    $qtdTotal.textContent = totalConvidados;
   }
-  $qtdConfirmados.textContent = count;
-}
 
-function listReloader() {
-  Array.prototype.forEach.call($checks, (check) => {
-    check.addEventListener("click", function () {
-      accountantChecked($checks);
-    });
-  });
-  Array.prototype.forEach.call($namesList, (nameList) => {
-    nameList.addEventListener("click", function (e) {
-      if (e.target.classList.value === "textTable") {
-        $listaConvidados.lastElementChild.parentNode.removeChild(this);
-        alert(`O convidado ${this.textContent} foi removido!`);
+  function accountantChecked(list) {
+    let x = 0;
+    let count = 0;
+
+    while (list.length > x) {
+      if (list[x].checked === true) {
+        count++;
       }
-      accountantGuests($checks);
-      accountantChecked($checks);
+      x++;
+    }
+    $qtdConfirmados.textContent = count;
+  }
+
+  function listReloader() {
+    Array.prototype.forEach.call($checks, (check) => {
+      check.addEventListener("click", function () {
+        accountantChecked($checks);
+      });
     });
-  });
-}
+    Array.prototype.forEach.call($namesList, (nameList) => {
+      nameList.addEventListener("click", function (e) {
+        if (e.target.classList.value === "textTable") {
+          try{
+            removeConvidado(e.target.parentNode.querySelector('.invisible').textContent)
+            $listaConvidados.lastElementChild.parentNode.removeChild(this);
+            alert(`O convidado ${this.textContent} foi removido!`);
+          }catch(err){
+            console.log(err)
+          }       
+        }
+        accountantGuests($checks);
+        accountantChecked($checks);
+      });
+    });
+  }
+  function montaHTML(nomeConvidado , confirmado, id){
+    let nome = document.createTextNode(nomeConvidado);
+    let tr = document.createElement("tr");
+    let th = document.createElement("th");
+    let td = document.createElement("td");
+    let idTd = document.createElement("td");
+    let checkbox = document.createElement("input");
+    
+    
+    idTd.textContent = id;
+    $listaConvidados.appendChild(tr);
+    tr.appendChild(th);
+    tr.appendChild(td);
+    th.appendChild(idTd);
+    idTd.classList.add("invisible");
+    th.classList.add("textTable");
+    tr.classList.add("namesList");
+    td.classList.add("text-center");
+    checkbox.type = "checkbox";
+    checkbox.classList.add("checks");
+    checkbox.checked = confirmado;
+    th.appendChild(nome);
+    td.appendChild(checkbox);
+    accountantGuests($checks);
+    accountantChecked($checks);
+    listReloader();
+    $inputNomeConvidado.value = "";
+    $inputConfirmado.checked = false;
+  }
+
+  function removeConvidado(id){
+    referenciaBanco.child(id)
+    .remove()
+    .then(function () {
+      console.log(id + " deletado com sucesso");
+    })
+    .catch((error) => {
+      console.log(Erro + error);
+    });
+  }
+})();
+
--- js/playlist.js
+++ js/playlist.js
@@ -1,115 +1,118 @@
-let $btnSalvarPlaylist = document.getElementById("btnSalvarPlaylist");
-let $inputNomeMusica = document.getElementById("inputNomeMusica");
-let $inputMomentoMusica = document.getElementById("inputMomentoMusica");
-let $inputLinkMusica = document.getElementById("inputLinkMusica");
-let $listaMusicas = document.getElementById("listaMusicas");
-let $musicasList = document.getElementsByClassName("musicasList");
-let url_atual1 = window.location.href.split("?");
-let idEvento = url_atual1[1];
-let listMusica = [];
+(function () {
+  let $btnSalvarPlaylist = document.getElementById("btnSalvarPlaylist");
+  let $inputNomeMusica = document.getElementById("inputNomeMusica");
+  let $inputMomentoMusica = document.getElementById("inputMomentoMusica");
+  let $inputLinkMusica = document.getElementById("inputLinkMusica");
+  let $listaMusicas = document.getElementById("listaMusicas");
+  let $musicasList = document.getElementsByClassName("musicasList");
+  let url_atual1 = window.location.href.split("?");
+  var idEvento = url_atual1[1];
+  let listMusica = [];
 
-window.onload = selectDatesMusic()
+  window.onload = selectDatesMusic()
 
-$btnSalvarPlaylist.addEventListener("click", function () {
-  let nomeMusica = $inputNomeMusica.value;
-  let momentoMusica = $inputMomentoMusica.value;
-  let linkMusica = $inputLinkMusica.value;
-  if(linkMusica[0].toLowerCase() != 'h' || linkMusica[1].toLowerCase() != 't' || linkMusica[2].toLowerCase() != 't' || linkMusica[3].toLowerCase() != 'p'){
-    linkMusica = `http://${linkMusica}`
-  }
-  montaHTML(nomeMusica , momentoMusica , linkMusica);
-  limpaCampos($inputNomeMusica , $inputMomentoMusica , $inputLinkMusica)
-  insertBanco(nomeMusica, momentoMusica, linkMusica);
-  listReloaderList();
-});
-
-function selectDatesMusic(){
-  let referenciaBanco = firebase.database()
-  .ref("EventoNovo"+"/"+idEvento+"/"+"playlist");
-  referenciaBanco.once("value").then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var childData = childSnapshot.val();
-      montaHTML(childData.musica , childData.momento , childData.link);
-      listReloaderList();
-    });
-
+  $btnSalvarPlaylist.addEventListener("click", function () {
+    let nomeMusica = $inputNomeMusica.value;
+    let momentoMusica = $inputMomentoMusica.value;
+    let linkMusica = $inputLinkMusica.value;
+    if(linkMusica[0].toLowerCase() != 'h' || linkMusica[1].toLowerCase() != 't' || linkMusica[2].toLowerCase() != 't' || linkMusica[3].toLowerCase() != 'p'){
+      linkMusica = `http://${linkMusica}`
+    }
+    montaHTML(nomeMusica , momentoMusica , linkMusica);
+    limpaCampos($inputNomeMusica , $inputMomentoMusica , $inputLinkMusica)
+    insertBanco(nomeMusica, momentoMusica, linkMusica);
+    listReloaderList();
   });
-}
 
-function listReloaderList() {
-  Array.prototype.forEach.call($musicasList, (musicasList) => {
-    musicasList.addEventListener("click", function (e) {
-      if (e.target.classList.value === "textTable") {
-        $listaMusicas.lastElementChild.parentNode.removeChild(this);
-        removeMusica(e.target.textContent)
-        alert(`A musica ${e.target.textContent} foi removida!`);
+  function selectDatesMusic(){
+    let referenciaBanco = firebase.database()
+    .ref("EventoNovo"+"/"+idEvento+"/"+"playlist");
+    referenciaBanco.once("value").then(function(snapshot) {
+      if(snapshot.exists()){
+        snapshot.forEach(function(childSnapshot) {
+          var childData = childSnapshot.val();
+          montaHTML(childData.musica , childData.momento , childData.link);
+          listReloaderList();
+        });
       }
     });
-  });
-}
+  }
 
-function insertBanco(musica, momento, link) {
-  let musicaSemEspaço = joinMusic(musica);
-  let referenciaBanco = firebase
+  function listReloaderList() {
+    Array.prototype.forEach.call($musicasList, (musicasList) => {
+      musicasList.addEventListener("click", function (e) {
+        if (e.target.classList.value === "textTable") {
+          $listaMusicas.lastElementChild.parentNode.removeChild(this);
+          removeMusica(e.target.textContent)
+          alert(`A musica ${e.target.textContent} foi removida!`);
+        }
+      });
+    });
+  }
+
+  function insertBanco(musica, momento, link) {
+    let musicaSemEspaço = joinMusic(musica);
+    let referenciaBanco = firebase
+      .database()
+      .ref("EventoNovo" + "/" + idEvento + "/" + "playlist/"+musicaSemEspaço+"/");
+    let data = { musica, momento, link };
+    referenciaBanco.update(data);
+  }
+
+  function removeMusica(musica){
+    let musicaSemEspaço = joinMusic(musica);
+    let referenciaBanco = firebase
     .database()
-    .ref("EventoNovo" + "/" + idEvento + "/" + "playlist/"+musicaSemEspaço+"/");
-  let data = { musica, momento, link };
-  referenciaBanco.update(data);
-}
+    .ref("EventoNovo" + "/" + idEvento + "/" + "playlist/");
 
-function removeMusica(musica){
-  let musicaSemEspaço = joinMusic(musica);
-  let referenciaBanco = firebase
-  .database()
-  .ref("EventoNovo" + "/" + idEvento + "/" + "playlist/");
+    referenciaBanco.child(musicaSemEspaço)
+    .remove()
+    .then(function () {
+      console.log(musica + " deletado com sucesso");
+    })
+    .catch((error) => {
+      console.log(Erro + error);
+    });
+  }
 
-  referenciaBanco.child(musicaSemEspaço)
-  .remove()
-  .then(function () {
-    console.log(musica + " deletado com sucesso");
-  })
-  .catch((error) => {
-    console.log(Erro + error);
-  });
-}
+  function joinMusic(musica){
+    let musicaSemEspaço = musica.split(" ");
+    musicaSemEspaço = musicaSemEspaço.join("");
+    return musicaSemEspaço;
+  }
 
-function joinMusic(musica){
-  let musicaSemEspaço = musica.split(" ");
-  musicaSemEspaço = musicaSemEspaço.join("");
-  return musicaSemEspaço;
-}
+  function montaHTML(musica1 , momento1 , link){
+    let musica = document.createTextNode(musica1);
+    let momento = document.createTextNode(momento1);
 
-function montaHTML(musica1 , momento1 , link){
-  let musica = document.createTextNode(musica1);
-  let momento = document.createTextNode(momento1);
+    let tr = document.createElement("tr");
+    let th = document.createElement("th");
+    let td = document.createElement("td");
+    let td2 = document.createElement("td");
+    let a = document.createElement("a");
+    let linkIcon = document.createElement("i");
 
-  let tr = document.createElement("tr");
-  let th = document.createElement("th");
-  let td = document.createElement("td");
-  let td2 = document.createElement("td");
-  let a = document.createElement("a");
-  let linkIcon = document.createElement("i");
+    $listaMusicas.appendChild(tr);
+    tr.appendChild(th);
+    tr.appendChild(td);
+    tr.appendChild(td2);
 
-  $listaMusicas.appendChild(tr);
-  tr.appendChild(th);
-  tr.appendChild(td);
-  tr.appendChild(td2);
+    th.classList.add("text-center");
+    tr.classList.add("musicasList", "text-center");
+    td.classList.add("textTable");
+    td2.classList.add("text-center");
+    linkIcon.classList.add("fas", "fa-volume-up");
+    th.appendChild(a);
+    a.href =  link;
+    a.target = "_blank";
+    a.appendChild(linkIcon);
+    td.appendChild(musica);
+    td2.appendChild(momento);
+  }
 
-  th.classList.add("text-center");
-  tr.classList.add("musicasList", "text-center");
-  td.classList.add("textTable");
-  td2.classList.add("text-center");
-  linkIcon.classList.add("fas", "fa-volume-up");
-  th.appendChild(a);
-  a.href =  link;
-  a.target = "_blank";
-  a.appendChild(linkIcon);
-  td.appendChild(musica);
-  td2.appendChild(momento);
-}
-
-function limpaCampos(campo1 , campo2 , campo3){
-  campo1.value = "";
-  campo2.value = "";
-  campo3.value = "";
-}
+  function limpaCampos(campo1 , campo2 , campo3){
+    campo1.value = "";
+    campo2.value = "";
+    campo3.value = "";
+  }
+})();
