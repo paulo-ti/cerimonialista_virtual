3b96aee3d99f1cc3e3f50148a3ffea27ffa51c70
paulo-ti <paulo-ti@hotmail.com>
2020-06-15 17:56:30 -0300

    edition on select states and citys more date

--- js/casamento.js
+++ js/casamento.js
@@ -8,23 +8,69 @@ verificarUsuarioLogado();
   var uNumeroConvidados;
   var url_atual = window.location.href.split('?');
   var referenciaBanco = firebase.database().ref("EventoNovo");
+  let $selectEstado = document.getElementById('selectEstado');
+  let $selectCidade = document.getElementById('selectCidade');
+  let $dataCasamento = document.getElementById('dataCasamento');
+  populateStateSelect()
+
+  window.addEventListener('load' , function(){
+    debugger
+    let date = new Date;
+    let month = date.getMonth()+1;
+    let day = date.getDate()+1;
+    if(month < 10) month = "0"+month
+    if(day < 10) day = "0"+day
+    date = `${date.getFullYear()}-${month}-${day}`
+    $dataCasamento.min = date;
+  })
+
+  $selectEstado.addEventListener('change' , function(e){
+    let nodesSelects = $selectCidade.childNodes;
+    [...nodesSelects].map( node => node.remove())
+    let estadoSelected = this.value;
+    fetch('/json/estados-cidades.json')
+    .then(res => res.json())
+    .then(states =>{
+      
+      let estado = states.estados.find( state => state.sigla === estadoSelected);
+        estado.cidades.map( (cidade) =>{
+          $selectCidade.disabled = false
+          const option = document.createElement('option');
+          option.setAttribute("value" ,cidade )
+          option.textContent = cidade;
+          $selectCidade.appendChild(option)
+        })
+    })
+  })
+
+  function populateStateSelect(){
+    fetch('/json/estados-cidades.json')
+    .then(res => res.json())
+    .then(states =>{
+      states.estados.map( state =>{
+        const option = document.createElement("option");
+        option.setAttribute("value" , state.sigla);
+        option.textContent = state.nome;
+        $selectEstado.appendChild(option);
+      })
+    })
+  }
+
   
   $btnInicioCasamento.addEventListener('click' , (e)=>{
     e.preventDefault();
     uNoivo1 = recuperaDados('noivo1').value;
     uNoivo2 = recuperaDados('noivo2').value;
     uDataCasamento = recuperaDados('dataCasamento').value;
-    uEstado = recuperaDados('estado').value;
-    uCidade = recuperaDados('cidade').value;
+    uEstado = recuperaDados('selectEstado').value;
+    uCidade = recuperaDados('selectCidade').value;
     uNumeroConvidados = recuperaDados('numeroConvidados').value;
 
     cadastraEvento(validaCampos());
   });
 
   function cadastraEvento(verificacao){
-    
     if(verificacao){
-      
        var resultato = referenciaBanco.push({
         noivo1: uNoivo1,
         noivo2 : uNoivo2,
@@ -60,8 +106,8 @@ verificarUsuarioLogado();
     validacao.push(testaCampo(recuperaDados('noivo1')));
     validacao.push(testaCampo(recuperaDados('noivo2')));
     validacao.push(testaCampo(recuperaDados('dataCasamento')));
-    validacao.push(testaCampo(recuperaDados('estado')));
-    validacao.push(testaCampo(recuperaDados('cidade')));
+    validacao.push(testaCampo(recuperaDados('selectEstado')));
+    validacao.push(testaCampo(recuperaDados('selectCidade')));
     validacao.push(testaCampo(recuperaDados('numeroConvidados')));
     validacao.filter( (teste) => {
       if(!teste){
